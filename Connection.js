b288f26fc85f8bacc59ee7e84268cbd3ae0a7859
Mohammed Zeyad Abu Raida <mohammedreda814@gmail.com>
2022-04-18 11:48:50 +0300

    Adding code P2P network in blockchain 'Decentralized BlockChain Development'

---
+++ Connection.js
@@ -0,0 +1,18 @@
+const WS = require("ws");
+const server = new WS.Server({ port: 8000 });
+server.on("connection", async (socket, req)=>{
+
+})
+
+const socket = new WS();
+socket.on("open", ()=>{
+
+})
+
+socket.on("close", ()=>{
+
+})
+
+socket.on("message", message=>{
+
+})
